2a10dc7b514bff2d03a4e5cc4eae5f8880d6b70f
dsouths <dsouths@hotmail.com>
2023-07-05 19:00:57 +0000

    loaded products & categories

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dsouths-boutiqueado-70w1wcjen5p.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['8000-dsouths-boutiqueado-sbtjuigcyqa.ws-eu101.gitpod.io']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
